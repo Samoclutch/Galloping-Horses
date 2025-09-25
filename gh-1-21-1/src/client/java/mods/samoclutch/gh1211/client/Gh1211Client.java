fb2885f36a711012252ea07193e956677d0d487d
Samoclutch <samoclutch@gmail.com>
2025-09-24 22:01:16 -0400

    1.21.1 version initial

---
+++ gh-1-21-1/src/client/java/mods/samoclutch/gh1211/client/Gh1211Client.java
@@ -0,0 +1,10 @@
+package mods.samoclutch.gh1211.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Gh1211Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ gh-1-21-1/src/main/java/mods/samoclutch/gh1211/HorseRidingClientPlayer.java
@@ -0,0 +1,5 @@
+package mods.samoclutch.gh1211;
+
+public interface HorseRidingClientPlayer {
+    boolean gallopingHorses$getGallopInput();
+}
